869f9bde6449c91e778f2709e4f4bc716da59071
The Lizard <suzpaylor@gmail.com>
2018-11-25 22:00:51 -0700

    Friends Data added

---
+++ app/data/friends.js
@@ -0,0 +1,154 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the brady bunch friends
+// ===============================================================================
+
+var friends = [
+    {
+      name:"Alice",
+      photo:"https://en.wikipedia.org/wiki/Ann_B._Davis#/media/File:Ann_B._Davis_1977.jpg",
+      scores:[
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Mike Brady",
+      photo: "https://en.wikipedia.org/wiki/List_of_The_Brady_Bunch_characters#/media/File:Robert_Reed_1971.JPG",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Carol Brady",
+      photo: "https://en.wikipedia.org/wiki/List_of_The_Brady_Bunch_characters#/media/File:Carol_Brady.png",
+      scores: [
+        "3",
+        "2",
+        "1",
+        "3",
+        "2",
+        "1",
+        "3",
+        "2",
+        "1",
+        "5"
+      ]
+    },
+    {
+      name: "Greg Brady",
+      photo: "https://en.wikipedia.org/wiki/List_of_The_Brady_Bunch_characters#/media/File:The_Brady_Bunch_Barry_Williams_1973.jpg",
+      scores: [
+        "3",
+        "2",
+        "1",
+        "3",
+        "2",
+        "1",
+        "3",
+        "2",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Marcia Brady",
+      photo: "https://en.wikipedia.org/wiki/List_of_The_Brady_Bunch_characters#/media/File:Marcia,_Marcia,_Marcia.png",
+      scores: [
+        "5",
+        "4",
+        "3",
+        "2",
+        "1",
+        "5",
+        "4",
+        "3",
+        "2",
+        "1"
+      ]
+    },
+    {
+      name: "Peter Brady",
+      photo: "https://en.wikipedia.org/wiki/List_of_The_Brady_Bunch_characters#/media/File:The_Brady_Bunch_Christopher_Knight_1973.jpg",
+      scores: [
+        "5",
+        "4",
+        "3",
+        "2",
+        "1",
+        "5",
+        "4",
+        "3",
+        "2",
+        "1"
+      ]
+    }, 
+    {
+      name: "Jan Brady",
+      photo: "https://en.wikipedia.org/wiki/List_of_The_Brady_Bunch_characters#/media/File:The_Brady_Bunch_Eve_Plumb_1973.jpg",
+      scores: [
+        "5",
+        "4",
+        "3",
+        "2",
+        "1",
+        "5",
+        "4",
+        "3",
+        "2",
+        "1"
+      ]
+    },     
+    {
+    name: "Bobby Brady",
+    photo: "https://en.wikipedia.org/wiki/List_of_The_Brady_Bunch_characters#/media/File:The_Brady_Bunch_Mike_Lookinland_1973.jpg",
+    scores: [
+        "5",
+        "4",
+        "3",
+        "2",
+        "1",
+        "5",
+        "4",
+        "3",
+        "2",
+        "1"
+        ]
+      },
+    {
+      name: "Cindy Brady",
+      photo: "https://en.wikipedia.org/wiki/List_of_The_Brady_Bunch_characters#/media/File:The_Brady_Bunch_Susan_Olsen_1973.jpg",
+      scores: [
+        "5",
+        "4",
+        "3",
+        "2",
+        "1",
+        "5",
+        "4",
+        "3",
+        "2",
+        "1"
+      ]
+    }
+  ]
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friends;
