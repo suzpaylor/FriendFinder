b35727123236caf93004e94ed8f3c4a7799a2766
The Lizard <suzpaylor@gmail.com>
2018-11-26 21:43:42 -0700

    survey

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,4 @@
-// ===============================================================================
-// DATA
-// Below data will hold all of the friends
-// ===============================================================================
+//Friend Data Below
 
 var friends = [
     {name: "Alice",
@@ -33,5 +30,5 @@ var friends = [
     scores: ["1","4","5","2","2","5","2","4","3","4"]},
   ]
 
-// Note how we export the array. This makes it accessible to other files using require.
+// Export the array
 module.exports = friends;
